e24b0d2fa1fe87f6016a81e7981e93d3562495a9
Umair Ansari <mumair1992@gmail.com>
2018-04-04 22:52:06 +0500

    initial comment

---
+++ index.js
@@ -0,0 +1,20 @@
+const electron =  require('electron');
+const ffmpeg = require('fluent-ffmpeg')
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file:\\${__dirname}\\index.html`);
+
+});
+
+ipcMain.on("video:submit", (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    // console.log('video duration is : ',metadata.format.duration, " seconds")
+    mainWindow.webContents.send(
+      "video:metadata",
+       metadata.format.duration
+     );
+  });
+});
